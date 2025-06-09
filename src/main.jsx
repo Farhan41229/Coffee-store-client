55116cdb95d757e634c462dd19dcbaa739b1361b
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-06-09 07:54:54 +0600

    Components Made. Routes Done. Layout Made.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import './index.css';
+import App from './App.jsx';
+import HomeLayout from './layouts/HomeLayout.jsx';
+import Home from './components/Home.jsx';
+import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: HomeLayout,
+    children: [
+      { index: true, Component: Home },
+      { path: '/addcoffee', Component: AddCoffee },
+      { path: '/updatecoffee', Component: UpdateCoffee },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
