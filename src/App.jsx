a0aeb91b3e222034c089df16b5bd806951c429ef
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-06-09 07:16:46 +0600

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl text-center font-extrabold flex justify-center items-center min-h-screen">
+        Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
